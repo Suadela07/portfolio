0be33c519ded77791e4013ef52e5b0834ffe64a3
Crystal H <heavens_lil_angel17@hotmail.com>
2017-04-09 15:17:55 -0700

    added prism.js and edited work file directory names

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,3 +4,32 @@ $(function () {
         $('.flex-nav ul').toggleClass('open');
     });
 });
+
+// *** SMOOTH SCROLL *** //
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        
+        if( $(this).parents('nav').hasClass('flex-nav') ){
+
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+
+        }else {
+
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 50) // 50 is the height of the sticky nav...
+          }, 1000);
+          return false;
+
+        }
+      }
+    }
+  });
+});
